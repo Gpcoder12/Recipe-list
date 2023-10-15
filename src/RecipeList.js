dfe863ef3cc28a9ef87b73610bc238c0b45cd666
Gopolang <mogashoag@yahoo.com>
2023-10-15 06:17:22 -0700

    commit

---
+++ src/RecipeList.js
@@ -0,0 +1,20 @@
+// src/RecipeList.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const RecipeList = ({ recipes }) => {
+  return (
+    <div>
+      <h1>Recipes List</h1>
+      <ul>
+        {recipes.map((recipe) => (
+          <li key={recipe.id}>
+            <Link to={`/recipe/${recipe.id}`}>{recipe.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default RecipeList;
