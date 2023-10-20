34c2c94049d72c41d4cba4e03e7a94ace3145141
Gopolang <mogashoag@yahoo.com>
2023-10-20 04:10:11 -0700

    commit

--- src/recipe.js
+++ src/recipe.js
@@ -2,27 +2,27 @@
 const recipes = [
     {
       id: 1,
-      title: 'Recipe 1',
-      description: 'Description for Recipe 1',
-      ingredients: ['Ingredient 1', 'Ingredient 2'],
+      title: 'Herby steak&crispy potatoes',
+      description: 'Green Pesto,juicy Mixed-colour Tomato Salad&Crushed Pistachios',
+      ingredients: ['500g red-skinned potatoes','20g shelled unsalted pistachios','6 ripe medium mixed-colour tomatoes','300g higher welfare sirlion steak'],
       steps: ['Step 1', 'Step 2'],
-      imageUrl: 'https://example.com/recipe1.jpg',
+      imageUrl: 'https://img.jamieoliver.com/jamieoliver/recipe-database/152012100.jpg?tr=w-330',
     },
     {
       id: 2,
       title: 'Spaghetti Bolognase',
       description: 'spahgetti served with sauce made from tomatoes,minced beef,garlic,wine and herbs',
-      ingredients: ['spaghdhetti', 'minced beef,garlic,tomatoes'],
+      ingredients: ['spaghhetti', 'minced beef','garlic','tomatoes'],
       steps: ['Step 1', 'Step 2'],
       imageUrl: 'https://www.recipetineats.com/wp-content/uploads/2018/07/Spaghetti-Bolognese.jpg?resize=350,350',
     },
     {
-        id: 2,
-        title: 'Recipe 2',
+        id: 3,
+        title: 'Garlic Chicken',
         description: 'Description for Recipe 2',
-        ingredients: ['Ingredient 1', 'Ingredient 2'],
+        ingredients: ['4 cloves of Garlic', '250g baby spinach','1 heaped tespoon sumac 700g cheackpeas'],
         steps: ['Step 1', 'Step 2'],
-        imageUrl: 'https://example.com/recipe2.jpg',
+        imageUrl: '',
       },
     // Add more recipes as needed
   ];
